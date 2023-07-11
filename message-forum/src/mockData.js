9914d907cb9ad37285a682444b571adfea581403
Kevin Lusk <klusk@caesars.com>
2023-07-11 16:15:21 -0400

    More data

--- message-forum/src/mockData.js
+++ message-forum/src/mockData.js
@@ -2,11 +2,20 @@ export let mockData = {
     events: [
         {
             eventID: 1,
-            messages:{
+            messages: [
+            {
                 messageID: 1,
                 messageBody: 'Chicago Bulls to win Superbowl',
-                messageVotes: 0,
+                messageVotes: -9,
+                author: mockUsers.users[1].userID
+            },
+            {
+                messageID: 2,
+                messageBody: 'Chicago Bulls will go all the way to the playoffs!',
+                messageVotes: 7,
+                author: mockUsers.users[3].userID
             }
+            ]
         },
         {   
             eventID: 20,
@@ -15,21 +24,32 @@ export let mockData = {
                 messageID: 20,
                 messageBody: 'Diamondbacks will crush Red Sox',
                 messageVotes: 30,
+                author: mockUsers.users[0].userID
             },
             {
                 messageID: 21,
                 messageBody: 'No no no, Red Sox will win.',
-                messageVotes: 29
+                messageVotes: 29,
+                author: mockUsers.users[3].userID
             }
         ]
         },
         {
             eventID: 300,
-            messages:{
+            messages: [
+            {
                 messageID: 312,
                 messageBody: 'Bet on the Packers!',
                 messageVotes: -111,
+                author: mockUsers.users[2].userID
+            },
+            {
+                messageID: 333,
+                messageBody: 'Vote against the Bears, they are so bad this year.',
+                messageVotes: 133,
+                author: mockUsers.users[4].userID
             }
+            ]
         }
     ]
 }
